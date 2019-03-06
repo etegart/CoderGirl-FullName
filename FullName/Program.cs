3dd6edd9158bfea1c390e52d9f53c8b3040dcdef
etegart <etegart@gmail.com>
2019-03-05 22:59:38 -0600

    Update Program.cs

--- FullName/Program.cs
+++ FullName/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace FullName
 {
@@ -7,14 +7,33 @@ namespace FullName
         public static void Main()
         {
             // TODO: Ask the user for their first name and then their last name.
-
             // TODO: Call a method called FullName that returns their full name.
-
             // TODO: Output their full name.
 
+            Console.Write("What is your first name? ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("What is your last name? ");
+            string lastName = Console.ReadLine();
+
+            // FullName(firstName, lastName);
+             FullName(firstName, lastName);
+            //Console.Write(String.Format("{0} {1}", firstName, lastName));
             Console.ReadLine();
         }
 
-        // TODO: Create a method call FullName that takes the firstName and lastName as parameters.
+        //public static string FullName(string fName, string lName)
+        public static string FullName(string fName, string lName)
+        {
+           Console.WriteLine(fName + " " + lName);
+           return String.Concat(fName, lName);
+        }
+        
+
+
+            // TODO: Create a method call FullName that takes the firstName and lastName as parameters.
+        
+
+        
     }
 }
